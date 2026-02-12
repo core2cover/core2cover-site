319768446f53b58ce2a9be38e472b1742119c4b5
Om280404 <omnileshkarande@gmail.com>
2026-02-12 10:31:33 +0530

    sitemap and robots

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -12,6 +12,6 @@ export default function robots(): MetadataRoute.Robots {
         '/api' 
       ],
     },
-    sitemap: 'https://core2cover.vercel.app/sitemap.xml',
+    sitemap: 'https://core2cover.in/sitemap.xml',
   }
 }
--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -1,7 +1,7 @@
 import { MetadataRoute } from 'next'
  
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = 'https://core2cover.vercel.app';
+  const baseUrl = 'https://core2cover.in';
   return [
     { url: baseUrl, lastModified: new Date() },
     { url: `${baseUrl}/designers`, lastModified: new Date() },
